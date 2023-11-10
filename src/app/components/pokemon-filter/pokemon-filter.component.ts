5c158d68c5092efc76575cd5971cf4170bde5275
gonzalotorre <garciatorregonzalo@gmail.com>
2023-11-10 18:49:25 +0100

    Added project structure with some components, services and pages and added angular material.

---
+++ src/app/components/pokemon-filter/pokemon-filter.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-pokemon-filter',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './pokemon-filter.component.html',
+  styleUrl: './pokemon-filter.component.css'
+})
+export class PokemonFilterComponent {
+
+}
