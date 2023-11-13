b591e80ae994ebc7fd81ebc373f4a41305da4761
gonzalotorre <garciatorregonzalo@gmail.com>
2023-11-13 22:27:40 +0100

    Added menu.

--- src/app/pages/menu/menu.component.ts
+++ src/app/pages/menu/menu.component.ts
@@ -1,19 +1,27 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {MatSidenavModule} from '@angular/material/sidenav';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { HomeComponent } from '../home/home.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule, MatSidenavModule, HomeComponent, MatToolbarModule, MatIconModule],
+  imports: [
+    CommonModule,
+    MatSidenavModule,
+    HomeComponent,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatListModule,
+  ],
   templateUrl: './menu.component.html',
-  styleUrl: './menu.component.css'
+  styleUrl: './menu.component.css',
 })
 export class MenuComponent {
-
   isExpanded = true;
-
 }
