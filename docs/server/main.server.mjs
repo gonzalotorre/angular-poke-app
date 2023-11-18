d487bdb8718482952c47a2ea430caa3a33bfb9cd
gonzalotorre <garciatorregonzalo@gmail.com>
2023-11-18 12:49:44 +0100

    Build app to deploy it.

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-EU7IAXVT.mjs";import"./chunk-FXZX4YHN.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-5V7DFHC5.mjs";import"./chunk-STY75PER.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
