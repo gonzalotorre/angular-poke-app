e4f40fdbdd7a59733d022ca5a3907e1fc106b574
gonzalotorre <garciatorregonzalo@gmail.com>
2023-11-24 19:15:11 +0100

    Build app files

--- docs/server/main.server.mjs
+++ docs/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-IWFNW7VT.mjs";import"./chunk-STY75PER.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
+import{a}from"./chunk-OHWDLSZ2.mjs";import"./chunk-WXPUN52F.mjs";import"./chunk-OIJB6ZDO.mjs";import"./chunk-7SQWPGAN.mjs";import"./chunk-KRLCULJA.mjs";export{a as default};
